c86ef73b91ff5ebcf2aeb55cbfb111e794a88599
Cody Clark <codyclark@Codys-MacBook-Pro.local>
2018-01-23 20:08:40 -0500

    building out gulp tasks and sass files

--- gulpfile.js
+++ gulpfile.js
@@ -33,12 +33,65 @@ var notify = function(error) {
 };
 
 //Bundle settings
+//automatic bundling of browserify-based scripts
 var bundler = watchify(browserify({
   entries: ['./src/app.jsx'],
-  transform: [reactify],
+  transform: [reactify], //transforms jsx into js files
   extensions: ['.jsx'],
   debug: true,
   cache: {},
   packageCache: {},
   fullPaths: true
 }));
+
+//Bundle tasks
+//transform jsx to js then bundle evrthing 2gether into file called main.js
+//that will sit in the root
+function bundle() {
+  return bundler
+    .bundle()
+    .on('error', notify)
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('./'))
+}
+bundler.on('update', bundle);
+
+//Create bundle
+gulp.task('build', function() {
+  bundle()
+});
+
+//compile sass files from main.scss
+gulp.task('sass', function() {
+  gulp.src('./sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./'));
+});
+
+//live reload server settings
+//reloads browser automatically each time there is a change in our code
+gulp.task('serve', function(done) {
+  gulp.src('')
+    .pipe(server({
+      livereload: {
+        enable: true,
+        filter: function(filePath, cb) {
+          if (/main.js/.test(filePath)) { //test filter, checks compiled css+js files b4 reloading browser
+            cb(true)
+          } else if (/style.css/.test(filePath)) {
+            cb(true)
+          }
+        }
+      },
+      open: true
+    }));
+});
+
+//watch task for sass files and sets order in which all created tasks need to run
+gulp.task('watch', function() {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+//run tasks in specific order
+gulp.task('default', ['build', 'serve', 'sass', 'watch']);
