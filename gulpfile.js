0ffa0a2fcbf7b5559a88eae2b2b4eb0ccc9079c3
Cody Clark <codyclark@Codys-MacBook-Pro.local>
2018-01-23 19:50:15 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var notifier = require('node-notifier');
+var server = require('gulp-server-livereload');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+
+var notify = function(error) {
+  var message = 'In: ';
+  var title = 'Error';
+
+  if (error.description) {
+    title += error.description;
+  } else if (error.message) {
+    title += error.message;
+  }
+
+  if (error.filename) {
+    var file = error.filename.split('/');
+    message += file[file.length - 1];
+  }
+
+  if (error.lineNumber) {
+    message += '\nOn Line: ' + error.lineNumber;
+  }
+
+  notifier.notify({title: title, message: message});
+};
+
+//Bundle settings
+var bundler = watchify(browserify({
+  entries: ['./src/app.jsx'],
+  transform: [reactify],
+  extensions: ['.jsx'],
+  debug: true,
+  cache: {},
+  packageCache: {},
+  fullPaths: true
+}));
